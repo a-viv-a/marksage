79847161bf7ae473b37e1d36cf01469afbd717e5
Isaac <isaac@rubenfamily.com>
2023-07-31 18:19:48 -0700

    start using clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[command(author, about, version)]
+struct Cli {
+    #[arg(short, long)]
+    vault_path: String,
+
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    Archive {},
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+    println!("Vault Path: {}", args.vault_path);
+    println!("Command: {:?}", args.command);
 }
