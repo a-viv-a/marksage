86effce0e3fed8733d6786e48456c2cf69c88540
Isaac <isaac@rubenfamily.com>
2023-07-31 21:26:36 -0700

    proper error checking for directories

---
+++ src/archive.rs
@@ -0,0 +1,3 @@
+pub fn archive() {
+    println!("Archive");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,48 @@
+mod archive;
+
+use std::path::PathBuf;
+
+use archive::archive;
 use clap::{Parser, Subcommand};
+
+fn parse_path(arg: &str) -> Result<PathBuf, std::io::Error> {
+    let path = PathBuf::from(arg);
+    match path.try_exists() {
+        Ok(true) => Ok(path),
+        Ok(false) => Err(std::io::Error::new(
+            std::io::ErrorKind::NotFound,
+            format!("Path not found"),
+        )),
+        Err(e) => Err(e),
+    }
+}
 
 #[derive(Parser, Debug)]
 #[command(author, about, version)]
 struct Cli {
+    /// The path to the obsidian vault to operate on
     #[arg(short, long)]
-    vault_path: String,
+    #[clap(value_parser = parse_path)]
+    vault_path: PathBuf,
 
     #[command(subcommand)]
-    command: Option<Commands>,
+    command: Commands,
 }
 
 #[derive(Subcommand, Debug)]
 enum Commands {
+    /// Archive todos that have been entirely completed
     Archive {},
 }
 
 fn main() {
     let args = Cli::parse();
-    println!("Vault Path: {}", args.vault_path);
+    println!("Vault Path: {}", args.vault_path.display());
     println!("Command: {:?}", args.command);
+
+    match args.command {
+        Commands::Archive {} => {
+            archive();
+        }
+    }
 }
