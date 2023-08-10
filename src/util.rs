227ee3bceb4d22fbc8082b78974e91f7c2cb0520
Isaac <isaac@rubenfamily.com>
2023-08-09 18:29:21 -0700

    write unit tests

--- src/util.rs
+++ src/util.rs
@@ -1,4 +1,7 @@
+use indoc::indoc;
 use regex::Regex;
+
+use lazy_static::lazy_static;
 
 /// Returns a regex that matches markdown files if they contain the given tag
 ///
@@ -21,4 +24,66 @@ pub fn markdown_contains_tag(tag: &str) -> Result<Regex, regex::Error> {
         )
         .as_str(),
     )
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    lazy_static! {
+        static ref IS_TAGGED_TODO: Regex = markdown_contains_tag("todo").unwrap();
+    }
+
+    macro_rules! markdown_contains_tag_tests {
+      ($($name:ident: $document:expr)*) => {
+      $(
+          #[test]
+          fn $name() {
+              let document = indoc!($document);
+              let is_tagged = stringify!($name).starts_with("tagged");
+              assert_eq!(is_tagged, IS_TAGGED_TODO.is_match(document));
+          }
+      )*
+      }
+    }
+
+    markdown_contains_tag_tests! {
+      untagged_document: r#"
+        - [ ] test
+    "#
+      tagged_document: r#"
+        #todo #other
+        - [ ] test
+    "#
+      tagged_document_with_frontmatter: r#"
+        ---
+        title: test
+        ---
+        #todo #other
+        - [ ] test
+    "#
+      tagged_document_with_frontmatter_and_newlines: r#"
+        ---
+        title: test
+        ---
+
+        #other
+        #todo
+
+        - [ ] test
+    "#
+      untagged_document_with_tag_in_content: r#"
+        - [ ] #todo test
+    "#
+      untagged_document_with_tag_in_frontmatter: r#"
+        ---
+        title: #todo test
+        ---
+        - [ ] test
+    "#
+      tagged_document_with_subtag: r#"
+        #todo/subtag
+        - [ ] test
+    "#
+    }
 }
